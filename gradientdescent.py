4928562ae9205c00a5eb529f0f91323f7f9d0246
Seungin Lyu <seungin.lyu@students.olin.edu>
2017-12-08 21:06:39 -0500

    Add adagrad

--- gradientdescent.py
+++ gradientdescent.py
@@ -90,11 +90,43 @@ def gradient_descent_momentum(theta, alpha, gamma, num_iters):
 
     return theta, J_history
 
+def gradient_descent_adagrad(theta, alpha, gamma, num_iters):
+    J_history = np.zeros(shape=(num_iters, 3))
+    epsilon = 10 ** (-8)
+    alpha_lst = [0, 0]
+    for i in range(num_iters):
+
+        cost = compute_cost(theta)
+
+        elev1 = get_elevation(theta[0] + delta, theta[1])
+        elev2 = get_elevation(theta[0] - delta, theta[1])
+        elev3 = get_elevation(theta[0], theta[1] + delta)
+        elev4 = get_elevation(theta[0], theta[1] - delta)
+
+        J_history[i] = [ cost, theta[0], theta[1] ]
+        if cost <= 0: return theta, J_history
+
+        lat_slope = elev1 / elev2 - 1
+        lon_slope = elev3 / elev4 - 1
+
+        alpha_lst[0] += lat_slope ** 2
+        alpha_lst[1] += lon_slope ** 2
+        alpha_lat = alpha / np.sqrt(alpha_lst[0] + epsilon)
+        alpha_lon = alpha / np.sqrt(alpha_lst[1] + epsilon)
+
+        theta[0][0] = theta[0][0] - alpha_lat
+        theta[1][0] = theta[1][0] - alpha_lon
+
+    return theta, J_history
+
 theta = np.array([ [args.lat], [args.lon] ])
 theta, J_history = gradient_descent_vanilla(theta, args.alpha, args.gamma, args.iters)
 
 theta2 = np.array([ [args.lat], [args.lon] ])
 theta2, J_history2 = gradient_descent_momentum(theta2, args.alpha, args.gamma, args.iters)
+
+theta3 = np.array([ [args.lat], [args.lon] ])
+theta3, J_history3 = gradient_descent_adagrad(theta3, args.alpha, args.gamma, args.iters)
 
 
 with open(args.output+"vanilla", 'w', newline='') as csvfile:
@@ -115,6 +147,17 @@ with open(args.output+"momentum", 'w', newline='') as csvfile:
             writer.writerow([ weight[1], weight[2] ])
 
 plot(np.arange(args.iters), J_history2[:, 0])
+xlabel('Iterations')
+ylabel('Elevation')
+show()
+
+with open(args.output+"adagrad", 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for weight in J_history3:
+        if weight[1] != 0 and weight[2] != 0:
+            writer.writerow([ weight[1], weight[2] ])
+
+plot(np.arange(args.iters), J_history3[:, 0])
 xlabel('Iterations')
 ylabel('Elevation')
 show()
