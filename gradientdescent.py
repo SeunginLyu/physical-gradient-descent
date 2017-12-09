a27e5b3d136ac44519e7fb3d28e9b658f3106dad
Seungin Lyu <seungin.lyu@students.olin.edu>
2017-12-08 21:47:22 -0500

    Add mountain everest, and adagrad

--- gradientdescent.py
+++ gradientdescent.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--output', type=str, default='output.csv')
 parser.add_argument('--alpha', type=float, default=0.01)
 parser.add_argument('--gamma', type=float, default=0.99)
-parser.add_argument('--iters', type=int, default=1000)
+parser.add_argument('--iters', type=int, default=10000)
 parser.add_argument('lat', type=float)
 parser.add_argument('lon', type=float)
 parser.add_argument('tif', type=str)
@@ -20,14 +20,20 @@ args = parser.parse_args()
 
 src = rasterio.open(args.tif)
 band = src.read(1)
+prev_band = 0
 
 delta = 0.001
 
 
 
 def get_elevation(lat, lon):
-    vals = src.index(lon, lat)
-    return band[vals]
+    global prev_band
+    try:
+        vals = src.index(lon, lat)
+        prev_band = band[vals]
+        return band[vals]
+    except:
+        return prev_band
 
 def compute_cost(theta):
     lat, lon = theta[0], theta[1]
@@ -114,10 +120,41 @@ def gradient_descent_adagrad(theta, alpha, gamma, num_iters):
         alpha_lat = alpha / np.sqrt(alpha_lst[0] + epsilon)
         alpha_lon = alpha / np.sqrt(alpha_lst[1] + epsilon)
 
-        theta[0][0] = theta[0][0] - alpha_lat
-        theta[1][0] = theta[1][0] - alpha_lon
+        theta[0][0] = theta[0][0] - alpha_lat * lat_slope
+        theta[1][0] = theta[1][0] - alpha_lon * lon_slope
 
     return theta, J_history
+
+def gradient_descent_adadelta(theta, alpha, gamma, num_iters):
+    J_history = np.zeros(shape=(num_iters, 3))
+    epsilon = 10 ** (-8)
+    gama = 0.9
+    gama_lst = [0, 0]
+    for i in range(num_iters):
+
+        cost = compute_cost(theta)
+
+        elev1 = get_elevation(theta[0] + delta, theta[1])
+        elev2 = get_elevation(theta[0] - delta, theta[1])
+        elev3 = get_elevation(theta[0], theta[1] + delta)
+        elev4 = get_elevation(theta[0], theta[1] - delta)
+
+        J_history[i] = [ cost, theta[0], theta[1] ]
+        if cost <= 0: return theta, J_history
+
+        lat_slope = elev1 / elev2 - 1
+        lon_slope = elev3 / elev4 - 1
+
+        gama_lst[0] = gama * gama_lst[0] + (1-gama) * (lat_slope ** 2)
+        gama_lst[1] = gama * gama_lst[1] + (1-gama) * (lon_slope ** 2)
+        alpha_lat = alpha / np.sqrt(gama_lst[0] + epsilon)
+        alpha_lon = alpha / np.sqrt(gama_lst[1] + epsilon)
+
+        theta[0][0] = theta[0][0] - alpha_lat * lat_slope
+        theta[1][0] = theta[1][0] - alpha_lon * lon_slope
+
+    return theta, J_history
+
 
 theta = np.array([ [args.lat], [args.lon] ])
 theta, J_history = gradient_descent_vanilla(theta, args.alpha, args.gamma, args.iters)
@@ -127,6 +164,9 @@ theta2, J_history2 = gradient_descent_momentum(theta2, args.alpha, args.gamma, a
 
 theta3 = np.array([ [args.lat], [args.lon] ])
 theta3, J_history3 = gradient_descent_adagrad(theta3, args.alpha, args.gamma, args.iters)
+
+theta4 = np.array([ [args.lat], [args.lon] ])
+theta4, J_history4 = gradient_descent_adadelta(theta4, args.alpha, args.gamma, args.iters)
 
 
 with open(args.output+"vanilla", 'w', newline='') as csvfile:
@@ -158,6 +198,17 @@ with open(args.output+"adagrad", 'w', newline='') as csvfile:
             writer.writerow([ weight[1], weight[2] ])
 
 plot(np.arange(args.iters), J_history3[:, 0])
+xlabel('Iterations')
+ylabel('Elevation')
+show()
+
+with open(args.output+"adadelta", 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for weight in J_history4:
+        if weight[1] != 0 and weight[2] != 0:
+            writer.writerow([ weight[1], weight[2] ])
+
+plot(np.arange(args.iters), J_history4[:, 0])
 xlabel('Iterations')
 ylabel('Elevation')
 show()
